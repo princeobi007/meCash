46bad99d3b87499a96dcef3bb8fd1a5fc3153347
piwuorie <princewill.iwuorie@osimis.io>
2024-07-23 17:39:03 +0100

    initial commit.

---
+++ src/main/java/com/iroegbulam/princewill/mecash/MeCashRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.iroegbulam.princewill.mecash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeCashRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeCashRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iroegbulam/princewill/mecash/MeCashRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iroegbulam.princewill.mecash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeCashRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
